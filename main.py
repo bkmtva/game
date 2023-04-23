8a6d23213f993ec196d638af4aac7c2c7aceffb1
mistledi <bkmtova.diana@gmail.com>
2023-04-23 14:31:31 +0600

    bit refactoring

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ import random
 
 pygame.init()
 
-SCREEN = pygame.display.set_mode((1280, 650))
+#screen size
+SCREEN = pygame.display.set_mode((1200, 650))
 
-
-BG = pygame.image.load("img/Background.png")
+#loading backgound imgs for menu and play
+BG = pygame.image.load("img/manu_background.png")
+BG = pygame.transform.scale(BG, (1200, 650))
+BG2 = pygame.image.load("img/play_background.jpg")
+BG2 = pygame.transform.scale(BG2, (1200, 650))
 
 
 def get_font(size): 
     return pygame.font.Font("img/font.ttf", size)
 
 
+BORDERS_IMG = pygame.image.load("img/borders.png").convert_alpha()
+BORDERS_IMG = pygame.transform.scale(BORDERS_IMG, (830, 455))
 
-
-
-
+#images for each team
 ROCK_IMG = pygame.image.load("img/rock.png").convert_alpha()
 ROCK_IMG = pygame.transform.scale(ROCK_IMG, (15, 15))
 PAPER_IMG = pygame.image.load("img/paper.png").convert_alpha()
@@ -28,87 +32,75 @@ SCISSORS_IMG = pygame.image.load("img/scissors.png").convert_alpha()
 SCISSORS_IMG = pygame.transform.scale(SCISSORS_IMG, (15, 15))
 
 
-teams = ['rock', 'paper', 'scissors']
+team_names = ['rock', 'paper', 'scissors']
 
+clock = pygame.time.Clock()
 
 
 class RPS(object):
 
     def __init__(self):
-        self.team_name = random.choice(teams)
-        self.speed_x = 1
-        self.speed_y = 1
-        if self.team_name == 'rock':
-            self.target = 'scissors'
-        elif self.team_name == 'paper':
-            self.target = 'rock'
-        elif self.team_name == 'scissors':
-            self.target = 'paper'
-        self.obj_spawn()
-        self.rec = Rect(random.randrange(190, 990, 1), random.randrange(200, 500, 1), self.width, self.height)
-        
-
-    def obj_spawn(self):
+        self.team_name = random.choice(team_names)
         self.width = 15
         self.height = 15
 
-    
-    def obj_drawing(self):
+        # random direction for speed 1
+        self.speed_x = random.choice([1, -1])
+        self.speed_y = random.choice([1, -1])
+
+        self.set_target()
+
+        # random place (coordinates) for every gamer
+        self.rec = Rect(random.randrange(230, 960, 1), random.randrange(150, 450, 1), self.width, self.height)
+
+    def set_target(self):
         if self.team_name == 'rock':
             self.target = 'scissors'
-            # pygame.draw.rect(SCREEN, (0, 105, 176), (self.rec),3)
+        elif self.team_name == 'paper':
+            self.target = 'rock'
+        elif self.team_name == 'scissors':
+            self.target = 'paper'
+
+    def player_drawing(self):
+        self.set_target()
+        if self.team_name == 'rock':
             SCREEN.blit(ROCK_IMG, self.rec)
         elif self.team_name == 'paper':
-            
-            self.target = 'rock'
-            # pygame.draw.rect(SCREEN, (0, 255, 176), (self.rec),3)
             SCREEN.blit(PAPER_IMG, self.rec)
         elif self.team_name == 'scissors':
-            
-            self.target = 'paper'
-            # pygame.draw.rect(SCREEN, (135, 255, 0), (self.rec),3)
             SCREEN.blit(SCISSORS_IMG, self.rec)
 
 
-clock = pygame.time.Clock()
+def move_elements(players):
+    for player in players:
+            if not win_checker(players):
+                player.rec.x += player.speed_x
+                player.rec.y += player.speed_y
 
-
-def move_elements(gamers):
-
-    lngth = len(gamers)
-
-    for i in range(lngth):
-            element = gamers[i]
-            m = gamers[:i] + gamers[i:]
-            if not win_checker(gamers):
-                element.rec.x += element.speed_x
-                element.rec.y += element.speed_y
-                if (element.rec.left <= 200 and element.speed_x < 0)  or (element.rec.right >= 1000 and element.speed_x > 0):
-                        element.speed_x *= -1
+                #border collision
+                if (player.rec.left <= 230 and player.speed_x < 0)  or (player.rec.right >= 1000 and player.speed_x > 0):
+                        player.speed_x *= -1
         
-                if (element.rec.top <= 200 and element.speed_y < 0) or (element.rec.bottom >= 500 and element.speed_y > 0):
-                    element.speed_y *= -1
-             
+                if (player.rec.top <= 160 and player.speed_y < 0) or (player.rec.bottom >= 470 and player.speed_y > 0):
+                    player.speed_y *= -1
                 
-
+                # collision between gamers
                 tollerance = 7
-                for maybe_target in m:
-                    if element != maybe_target and element.rec.colliderect(maybe_target.rec):
-                        if maybe_target.team_name == element.target:
-                            maybe_target.team_name = element.team_name
-                        if abs(maybe_target.rec.top - element.rec.bottom) < tollerance and element.speed_y > 0:
-                            element.speed_y*=-1
-                        elif abs(maybe_target.rec.bottom - element.rec.top) < tollerance and element.speed_y < 0:
-                            element.speed_y*=-1
-                        elif abs(maybe_target.rec.right - element.rec.left) < tollerance and element.speed_x < 0:
-                            element.speed_x*=-1
-                        elif abs(maybe_target.rec.left - element.rec.right) < tollerance and element.speed_x > 0:
-                            element.speed_x*=-1
-                    
-                    
-                     
-               
-            element.obj_drawing()
+                for maybe_target in players:
+                    if maybe_target != player:
+                        if player != maybe_target and player.rec.colliderect(maybe_target.rec):
+                            if maybe_target.team_name == player.target:
+                                maybe_target.team_name = player.team_name
+                            if abs(maybe_target.rec.top - player.rec.bottom) < tollerance and player.speed_y > 0:
+                                player.speed_y*=-1
+                            elif abs(maybe_target.rec.bottom - player.rec.top) < tollerance and player.speed_y < 0:
+                                player.speed_y*=-1
+                            elif abs(maybe_target.rec.right - player.rec.left) < tollerance and player.speed_x < 0:
+                                player.speed_x*=-1
+                            elif abs(maybe_target.rec.left - player.rec.right) < tollerance and player.speed_x > 0:
+                                player.speed_x*=-1
+
+            player.player_drawing()
 
 
 rocks_score = 0
@@ -141,6 +133,8 @@ def win_checker(gamers):
 
 
 def drow_schield(team_name, total, score):
+    '''display the graphic and track shield for the gamers'''
+
     top = 20
     top_corner = 18.5
     top_name = 30
@@ -155,7 +149,8 @@ def drow_schield(team_name, total, score):
         top_corner += 80
         top_name += 80
         left_name += 14
-    persent, player_shield_color = shield_bar(total, score)
+        
+    persent, player_shield_color = get_persent_color(total, score)
     pygame.draw.rect(SCREEN, (220,220,220), (20, top_corner, 104, 24), 3)
     pygame.draw.rect(SCREEN, player_shield_color, (22, top, persent, 20))
     TEAM_NAME = get_font(10).render(team_name, True, "White")
@@ -163,139 +158,102 @@ def drow_schield(team_name, total, score):
     SCREEN.blit(TEAM_NAME, TEAM_RECT) 
 
 
-def shield_bar(total, player_shield):
-    '''display the graphic and track shield for the gamers'''
-
+def get_persent_color(total, player_shield):
     persent = player_shield*100//total
-
-    if persent >= 100:
+    if persent > 70:
         player_shield_color = (124,252,0)
-    elif persent > 75:
-        player_shield_color = (124,252,0)
-    elif persent > 50:
+    elif persent > 35:
         player_shield_color = (255,255,0)
     else:
         player_shield_color= (255,160,122)
     return persent, player_shield_color
 
 
+def play_button_controller(PLAY_MOUSE_POS):
+    PLAY_BACK = Button(image=None, pos=(820, 600), 
+                            text_input="BACK", font=get_font(50), base_color="White", hovering_color="Red")
+    PLAY_RESTART = Button(image=None, pos=(500, 600), 
+                        text_input="RESTART", font=get_font(50), base_color="White", hovering_color="Yellow")
 
+    PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+    PLAY_BACK.update(SCREEN)
+    PLAY_RESTART.changeColor(PLAY_MOUSE_POS)
+    PLAY_RESTART.update(SCREEN)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                main_menu()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if PLAY_RESTART.checkForInput(PLAY_MOUSE_POS):
+                play()
 
 def play():
+
+    pygame.display.set_caption("Play")
+    
+    #music
     pygame.mixer.music.pause()
     pygame.mixer.music.load('img/gigachad.mp3')
-
-    print("music started playing....")
-
-    #Set preferred volume
     pygame.mixer.music.set_volume(0.2)
-
-    #Play the music
     pygame.mixer.music.play()	
     
-    pygame.display.set_caption("Play")
-    gamers = []
-    n = random.randint(50, 300)
 
-    for i in range(n):
+    gamers = []
+    total_gamers_num = random.randint(50, 200)
+
+    for i in range(total_gamers_num):
         paper = RPS()
         gamers.append(paper)
     
-    total = len(gamers)
     
     while True:
         clock.tick(60)
+
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-
         
-        
+        #background
         SCREEN.fill("black")
-        
+        SCREEN.blit(BG2, (0, 0))
 
-        rec = Rect(200, 200, 800, 310)
-        pygame.draw.rect(SCREEN, (255, 255, 0), (rec),3)
-
+        rec = Rect(200, 88, 800, 360)
+        SCREEN.blit(BORDERS_IMG, rec)
 
         move_elements(gamers)
+
         winner = win_checker(gamers)
 
-        drow_schield("rocks", total, rocks_score)
-        drow_schield("papers", total, papers_score)
-        drow_schield("scissors", total, scissors_score)
+        drow_schield("rocks", total_gamers_num, rocks_score)
+        drow_schield("papers", total_gamers_num, papers_score)
+        drow_schield("scissors", total_gamers_num, scissors_score)
         
         PLAY_TEXT = get_font(40).render("Observe the game!" if not winner else "We have Winner!", True, "White")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 60))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
         if winner:
-            WON_TEXT = get_font(40).render(winner, True, "Yellow")
-            WON_RECT = PLAY_TEXT.get_rect(center=(715, 330))
+            WON_TEXT = get_font(40).render(winner, True, "Green")
+            WON_RECT = PLAY_TEXT.get_rect(center=(715, 300))
             SCREEN.blit(WON_TEXT, WON_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(820, 600), 
-                            text_input="BACK", font=get_font(50), base_color="White", hovering_color="Red")
-        PLAY_RESTART = Button(image=None, pos=(500, 600), 
-                            text_input="RESTART", font=get_font(50), base_color="White", hovering_color="Green")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
-        PLAY_RESTART.changeColor(PLAY_MOUSE_POS)
-        PLAY_RESTART.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    main_menu()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_RESTART.checkForInput(PLAY_MOUSE_POS):
-                    
-                    play()
-
+        play_button_controller(PLAY_MOUSE_POS)
         pygame.display.update()
         
-    
-def options():
-    pygame.display.set_caption("Options")
-    while True:
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("white")
-
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-
-        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
-                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
-
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
-
-        pygame.display.update()
-
 
 def main_menu():
-    pygame.mixer.music.load('img/music.mp3')
-
-    print("music started playing....")
-
-    #Set preferred volume
-    pygame.mixer.music.set_volume(0.2)
-
-    #Play the music
-    pygame.mixer.music.play()
     pygame.display.set_caption("Menu")
+
+    #music
+    pygame.mixer.music.load('img/music.mp3')
+    pygame.mixer.music.set_volume(0.2)
+    pygame.mixer.music.play()
+    
+
     while True:
+
+        #set background img
         SCREEN.blit(BG, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
@@ -305,14 +263,13 @@ def main_menu():
 
         PLAY_BUTTON = Button(image=pygame.image.load("img/Play Rect.png"), pos=(640, 250), 
                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("img/Options Rect.png"), pos=(640, 400), 
-                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("img/Quit Rect.png"), pos=(640, 550), 
+        
+        QUIT_BUTTON = Button(image=pygame.image.load("img/Quit Rect.png"), pos=(640, 400), 
                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+        for button in [PLAY_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
         
@@ -324,8 +281,6 @@ def main_menu():
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
 
-                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    options()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
